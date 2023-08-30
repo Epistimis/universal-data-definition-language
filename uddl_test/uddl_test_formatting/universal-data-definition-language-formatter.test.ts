58f6fd4eb3dc5859c7d540eec6e2920d83a0e136
maytheu <maytheu98@gmail.com>
2023-08-30 01:54:46 +0100

    test to formats conceptual data model

--- uddl_test/uddl_test_formatting/universal-data-definition-language-formatter.test.ts
+++ uddl_test/uddl_test_formatting/universal-data-definition-language-formatter.test.ts
@@ -1,20 +1,36 @@
 import { AstNode } from "langium";
 import { UniversalDataDefinitionLanguageFormatter } from "../../src/language-server/universal-data-definition-language-formatter";
-// import * as ast from "../../src/language-server/generated/ast";
+import * as ast from "../../src/language-server/generated/ast";
 
-/** Hav access to the protected class defined in the UniversalDataDefinitionLanguageFormatter class */
+/** Have access to the protected class defined in the UniversalDataDefinitionLanguageFormatter class */
 class UniversalDataDefinitionLanguageFormatterTest extends UniversalDataDefinitionLanguageFormatter {
-  formatContainerTest(node: AstNode):void {
-     this.formatContainer(node);
+  formatContainerTest(node: AstNode): void {
+    this.formatContainer(node);
+  }
+  formatTest(node: AstNode): void {
+    this.format(node);
   }
 }
 
 describe("UniversalDataDefinitionLanguageFormatter", () => {
   it("formats a container node", () => {
-    const container: AstNode = { $type: "" };
+    const container: AstNode = { $type: "Root container" };
     const formatter = new UniversalDataDefinitionLanguageFormatterTest();
     const mockFormatContainer = jest.spyOn(formatter, "formatContainerTest");
     formatter.formatContainerTest(container);
     expect(mockFormatContainer).toHaveBeenCalledWith(container);
   });
+
+  it("formats conceptual data model", () => {
+    // This expects a container deinition of ConceptualDataModel | DataModel | File | LogicalDataModel | LogicalEnumerated | LogicalEnumeratedSet | LogicalMeasurementSystem | LogicalValueTypeUnit | PlatformDataMode type
+      const cdmElementContainer: ast.ConceptualElement = { $type: 'ConceptualObservable', name: "Information", description: "Something a party can learn", $container: /*some types*/ }
+      const cdmContainer: ast.ConceptualDataModel = { $type: "ConceptualDataModel", name: "Conceptual", description: "Need to start at Conceptual Level", element: [cdmElementContainer], $container: /*come types*/, cdm: [] }
+      const  container:ast.DataModel  = { $type: "DataModel", cdm: [cdmContainer], name: "PPT", description: "Base data structures to support People, Places and Things", pdm: [], ldm: [], $container: cdmContainer,};
+
+    const formatter = new UniversalDataDefinitionLanguageFormatterTest();
+    const mockFormatContainer = jest.spyOn(formatter, "formatTest");
+    formatter.formatTest(container);
+
+  });
 });
+
